7b218c1338a2e6c17600f21a104ad0be961ebbf8
Omer80 <omertz@post.bgu.ac.il>
2018-12-13 18:45:29 +0200

    Trying to solve the stroboscope thing

--- stroboscope_model.py
+++ stroboscope_model.py
@@ -11,37 +11,50 @@ import sdeint
 from scipy.signal import find_peaks
 from BenincaModel import BenincaModel,Es_normal
 
-def find_timeseries_peaks(fname,var_index,ito,resolution,Tmax_index):
-    Tmax_array = np.linspace(17.5,25,100)
-    Tmax = Tmax_array[Tmax_index-1]
-    print("Calculating for Tmax=",Tmax)
-    m = BenincaModel(Es=Es_normal,Ps='auto/Beninca_set1.hdf5',Vs=None)
+
+def calc_for_constant(m):
     t,sol_const = m.ode_integrate([0.5,0.5,0.5,0.5])
-    init_cond = sol_const[-1]
-    int_finish=100*365
-    trim=20*365
+    return sol_const[-1]
+def calc_for_oscillation_with_Ito(m,init_cond,Tmax,ito,int_finish,step):
+    tspan = np.arange(0.0, int_finish+step,step)
+    forcing = -1.0*m.Ft(tspan)
     parameters={}
     parameters['Tmax']=Tmax
     m.update_parameters(parameters)
     def G(y, t):
         return np.array([[ito,0.0,0.0,0.0],[0.0,ito,0.0,0.0],[0.0,0.0,ito,0.0],[0.0,0.0,0.0,ito]])
-    step=0.01
-    tspan = np.arange(0.0, int_finish+step,step)
-    forcing = -1.0*m.Ft(tspan)
-    print("Integrating for initial conditions:",init_cond)
     result = sdeint.itoint(m.rhs_ode, G, init_cond, tspan)
+    return tspan,result,forcing
+
+def find_timeseries_peaks(fname,var_index,ito,resolution,Tmax_index):
+    Tmax_array = np.linspace(17.5,25,100)
+    Tmax = Tmax_array[Tmax_index-1]
+    print("Calculating for Tmax=",Tmax)
+    m = BenincaModel(Es=Es_normal,Ps='auto/Beninca_set1.hdf5',Vs=None)
+    init_cond = calc_for_constant(m)
+    int_finish=100*365
+    trim=20*365
+    step=0.01
+    print("Integrating for initial conditions:",init_cond)
+    _,result,forcing=calc_for_oscillation_with_Ito(m,init_cond,Tmax,ito,int_finish,step)
     peaks_signal, _=find_peaks(result[trim:,var_index])
     peaks_forcing, _=find_peaks(forcing[trim:])
     Tmax_peaks_signal = np.ones_like(peaks_signal)*Tmax
     sfname = fname+"var_"+str(var_index)+"_"+str(Tmax_index)+".dat"
-    np.savetxt(sfname,np.array([Tmax_peaks_signal,
-                                result[trim:,var_index][peaks_signal],
-                                result[trim:,var_index][peaks_forcing]]).T)
+    data = np.array([Tmax_peaks_signal,
+                     result[trim:,var_index][peaks_signal],
+                     result[trim:,var_index][peaks_forcing]]).T
+    return sfname,data
+
+def save_find_timeseries_peaks(fname,var_index,ito,resolution,Tmax_index):
+    sfname,data=find_timeseries_peaks(fname,var_index,ito,resolution,Tmax_index)
+    np.savetxt(sfname,data)
     print("Saved to",sfname)
     
 def main(args):
-    find_timeseries_peaks(args.fname,args.var_index,args.ito,
-                          args.resolution,args.Tmax_index)
+    if args.find_peaks:
+        save_find_timeseries_peaks(args.fname,args.var_index,args.ito,
+                              args.resolution,args.Tmax_index)
 
 def add_parser_arguments(parser):
     parser.add_argument("-f", "--fname",
@@ -54,6 +67,11 @@ def add_parser_arguments(parser):
                         dest="verbose",
                         default=False,
                         help="Turn on debuging messages")
+    parser.add_argument("--find_peaks",
+                        action="store_true",
+                        dest="find_peaks",
+                        default=False,
+                        help="Start find_peaks function")
     parser.add_argument('-i','--Tmax_index',
                         dest='Tmax_index',
                         type=int,
