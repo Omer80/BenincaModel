704cfbf86dd5ae46a381eb982096431281f78b6d
Omer80 <omertz@post.bgu.ac.il>
2018-12-13 10:36:57 +0200

    Before uploading to my desktop

--- stroboscope_model.py
+++ stroboscope_model.py
@@ -11,16 +11,15 @@ import sdeint
 from scipy.signal import find_peaks
 from BenincaModel import BenincaModel,Es_normal
 
-def find_timeseries_peaks(fname,resolution,Tmax_index):
+def find_timeseries_peaks(fname,var_index,ito,resolution,Tmax_index):
     Tmax_array = np.linspace(17.5,25,100)
-    Tmax = Tmax_array[Tmax_index]
+    Tmax = Tmax_array[Tmax_index-1]
     print("Calculating for Tmax=",Tmax)
     m = BenincaModel(Es=Es_normal,Ps='auto/Beninca_set1.hdf5',Vs=None)
     t,sol_const = m.ode_integrate([0.5,0.5,0.5,0.5])
     init_cond = sol_const[-1]
     int_finish=100*365
     trim=20*365
-    ito=0.0000
     parameters={}
     parameters['Tmax']=Tmax
     m.update_parameters(parameters)
@@ -31,17 +30,18 @@ def find_timeseries_peaks(fname,resolution,Tmax_index):
     forcing = -1.0*m.Ft(tspan)
     print("Integrating for initial conditions:",init_cond)
     result = sdeint.itoint(m.rhs_ode, G, init_cond, tspan)
-    peaks_signal, _=find_peaks(result[trim:,0])
+    peaks_signal, _=find_peaks(result[trim:,var_index])
     peaks_forcing, _=find_peaks(forcing[trim:])
     Tmax_peaks_signal = np.ones_like(peaks_signal)*Tmax
-    sfname = fname+str(Tmax_index)+".dat"
+    sfname = fname+"var_"+str(var_index)+"_"+str(Tmax_index)+".dat"
     np.savetxt(sfname,np.array([Tmax_peaks_signal,
-                                result[trim:,0][peaks_signal],
-                                result[trim:,-2][peaks_forcing]]).T)
+                                result[trim:,var_index][peaks_signal],
+                                result[trim:,var_index][peaks_forcing]]).T)
     print("Saved to",sfname)
     
 def main(args):
-    find_timeseries_peaks(args.fname,args.resolution,args.Tmax_index)
+    find_timeseries_peaks(args.fname,args.var_index,args.ito,
+                          args.resolution,args.Tmax_index)
 
 def add_parser_arguments(parser):
     parser.add_argument("-f", "--fname",
@@ -57,8 +57,18 @@ def add_parser_arguments(parser):
     parser.add_argument('-i','--Tmax_index',
                         dest='Tmax_index',
                         type=int,
+                        default=1,
+                        help='Index to point at index at Tmax array - minimum 1')
+    parser.add_argument('--var_index',
+                        dest='var_index',
+                        type=int,
                         default=0,
-                        help='Index to point at index at Tmax array')
+                        help='Index of variable from 0 to 3')
+    parser.add_argument('--ito',
+                        dest='ito',
+                        type=float,
+                        default=0.0,
+                        help='Ito diagonal noise')
     parser.add_argument('--resolution',
                         dest='resolution',
                         type=int,
